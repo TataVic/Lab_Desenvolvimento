d85b0d153d07ed463b2e1a437ddb767cd6fe01ff
Tauani Vitória <tauanivitoria1302@gmail.com>
2024-02-09 21:31:25 -0300

    realizado teste e encabeçalhamentos dos processos do método get

--- Aula 1/ajax/get/index.php
+++ Aula 1/ajax/get/index.php
@@ -6,6 +6,27 @@
     <title>Exemplo Ajax - Get</title>
 </head>
 <body>
+    <h1>Exemplo de AJAX</h1>
+    <p>Clique no botão para carregar os dados</p>
+    <button onclick="carregar()">Carregar dados</button>
+    <div id="resultados"> 
+        
+    </div>
+<script type="text/javascript">
+    function carregar(){
+        var link = new XMLHttpRequest();
+        link.open("GET","dados.json", true);
+        link.onload = function(){
+            if(link.status === 200){
+                var div = document.getElementById("resultados");
+            }
+            else{alert("Erro ao carregar dados"+link.statusText);}
+        }
+    };
+    link.onerror = function(){
+        alert("Erro na consulta de dados!")
+    }
+</script>
     
 </body>
 </html>
